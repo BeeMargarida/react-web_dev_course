c4be9a2724818bcb91623d038477e8164ad40462
beemargarida <up201505505@gcloud.fe.up.pt>
2018-06-18 18:47:12 +0000

    recipe add with state done

--- recipe-with-state/src/Navbar.js
+++ recipe-with-state/src/Navbar.js
@@ -1,7 +1,16 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import './Navbar.css';
 
 class Navbar extends Component {
+    
+    static defaultProps = {
+        onNewRecipe(){}
+    }
+    
+    static propTypes = {
+        onNewRecipe: PropTypes.func
+    }
     
     render() {
         
@@ -9,7 +18,7 @@ class Navbar extends Component {
             <header>
                 <h2><a>Recipe App</a></h2>
                 <nav>
-                    <li><a>New Recipe</a></li>
+                    <li><a onClick={this.props.onNewRecipe}>New Recipe</a></li>
                     <li><a>Home</a></li>
                     <li><a>About</a></li>
                     <li><a>Contact Us</a></li>
